d52c52311567929e6c0c1f254dcba20d77130ee5
Hassaan Yousafzai <p200088@pwr.nu.edu.pk>
2023-07-21 02:21:02 +0500

    markers and animation added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,6 +13,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   MapType _currentMapType = MapType.normal;
 
+  final List<Marker> _marker = [];
+
+  final List<Marker> _markerList = [
+    const Marker(
+      markerId: MarkerId('1'),
+      position: LatLng(33.9994409, 71.5179113),
+      infoWindow: InfoWindow(title: 'Peshawar Cantt'),
+    ),
+    const Marker(
+      markerId: MarkerId('2'),
+      position: LatLng(34.028872, 71.533497),
+      infoWindow: InfoWindow(title: 'Warsak Road'),
+    ),
+    const Marker(
+      markerId: MarkerId('3'),
+      position: LatLng(51.528607, -0.4312472),
+      infoWindow: InfoWindow(title: 'London'),
+    ),
+  ];
+
   void changeMapType() {
     setState(() {
       _currentMapType =
@@ -20,11 +40,18 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  @override
+  void initState() {
+    super.initState();
+
+    _marker.addAll(_markerList);
+  }
+
   final Completer<GoogleMapController> _controller = Completer();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    target: LatLng(33.9994409, 71.5179113),
+    zoom: 14,
   );
 
   @override
@@ -38,6 +65,7 @@ class _HomePageState extends State<HomePage> {
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
             },
+            markers: Set.of(_marker),
           ),
           Align(
             alignment: Alignment.bottomCenter,
@@ -67,6 +95,40 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   ),
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20, left: 20),
+              child: InkWell(
+                onTap: () async {
+                  GoogleMapController controller = await _controller.future;
+                  controller.animateCamera(
+                    CameraUpdate.newCameraPosition(
+                      const CameraPosition(
+                        target: LatLng(51.528607, -0.4312472),
+                        zoom: 14,
+                      ),
+                    ),
+                  );
+                  setState(() {});
+                },
+                child: Container(
+                  height: 60,
+                  width: 60,
+                  decoration: _currentMapType == MapType.normal
+                      ? BoxDecoration(
+                          color: Colors.yellow,
+                          borderRadius: BorderRadius.circular(30),
+                        )
+                      : BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                  child: const Center(child: Icon(Icons.my_location)),
                 ),
               ),
             ),
